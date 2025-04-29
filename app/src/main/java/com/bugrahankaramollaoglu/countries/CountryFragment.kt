95449030239ae40d901d7b29d9e918b5928ef964
bugrahankaramollaoglu <bugrahankaramollaoglu@gmail.com>
2025-04-29 19:40:18 +0300

    will move structures

--- app/src/main/java/com/bugrahankaramollaoglu/countries/CountryFragment.kt
+++ app/src/main/java/com/bugrahankaramollaoglu/countries/CountryFragment.kt
@@ -9,6 +9,8 @@ import android.view.ViewGroup
 
 class CountryFragment : Fragment() {
 
+    private var countryUuid = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -20,6 +22,15 @@ class CountryFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_country, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            countryUuid = CountryFragmentArgs.fromBundle(it).countryUuid
+        }
+
     }
 
 }
